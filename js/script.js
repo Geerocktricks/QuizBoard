53b05266ef60b667ef2df9aa89fde49aeb40b74d
Geerocktricks <geerockface4@gmail.com>
2018-09-22 13:11:26 +0300

    This commit adds progress bar to my Quizboard page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-  $("#back-1").click(funtion(){
-    $(".details1").show();
-    $(".details").hide();
-  });
+  $(#Next1).click(function(){
+    $(second-p).show();
+  })
 });
